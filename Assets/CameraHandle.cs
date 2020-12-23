424a5d29952eb57aeb9f697ae981953db20137ca
birdboiboi <jai36@drexel.edu>
2020-12-23 18:26:26 -0500

    added basic player controls

---
+++ Assets/CameraHandle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandle : MonoBehaviour
+{
+    public float sensitiveity = 100;
+    public Transform body;
+    private float xRot= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yaw = Input.GetAxis("Mouse Y") * sensitiveity * Time.deltaTime;
+        float pitch = Input.GetAxis("Mouse X") * sensitiveity * Time.deltaTime;
+        Debug.Log(yaw + "yaw:pitch" + pitch);
+        xRot -= yaw;
+
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        body.Rotate(pitch * Vector3.up);
+
+
+    }
+}
---
+++ Assets/CharMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = gameObject.GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        
+
+     
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
+
